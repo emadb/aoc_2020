0229e99f61efa6d3e2d6ebbaafa30c00ae9c8c3c
ema <emanuele@codiceplastico.com>
2020-12-01 11:04:55 +0100

    refactoring

--- lib/day_one.ex
+++ lib/day_one.ex
@@ -2,32 +2,25 @@ defmodule Aoc.DayOne do
 
   def part_one(file_path) do
     input = get_input_list(file_path)
-    couples = for a <- input, b <- input do
-      {a, _} = Integer.parse(a)
-      {b, _} = Integer.parse(b)
-      {a, b}
-    end
-    {a, b} = Enum.find(couples, fn {a, b} -> a + b == 2020 end)
-    a * b
+    List.first(for a <- input, b <- input, is_2020(a, b), do: a * b)
   end
 
   def part_two(file_path) do
     input = get_input_list(file_path)
-    couples = for a <- input, b <- input, c <- input do
-      {a, _} = Integer.parse(a)
-      {b, _} = Integer.parse(b)
-      {c, _} = Integer.parse(c)
-      {a, b, c}
-    end
-    {a, b, c} = Enum.find(couples, fn {a, b, c} -> a + b + c == 2020 end)
-    a * b * c
+    List.first(for a <- input, b <- input, c <- input, is_2020(a, b, c), do: a * b * c)
   end
 
   def get_input_list(file_path) do
     file_path
     |> File.read!()
     |> String.split("\n")
+    |> Enum.map(fn i ->
+      {n, _} = Integer.parse(i)
+      n
+    end)
+
   end
 
+  defp is_2020(a, b, c \\ 0), do: a + b + c == 2020
 
 end
