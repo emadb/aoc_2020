e51afa17de868bbc9bd6c7434efb7cfacdc2e3bf
ema <emanuele@codiceplastico.com>
2020-12-03 14:16:09 +0100

    day 3 part 2

--- test/day_three_test.exs
+++ test/day_three_test.exs
@@ -1,16 +1,29 @@
 defmodule Aoc.DayThreeTest do
   use ExUnit.Case
+  @fake_input "./test/fake_inputs/day_three.txt"
+  @real_input "./lib/inputs/day_three.txt"
 
   test "part one (fake)" do
-    input = Path.absname("./test/fake_inputs/day_three.txt")
+    input = Path.absname(@fake_input)
     result = Aoc.DayThree.part_one(input)
     assert result == 7
   end
 
   test "part one" do
-    input = Path.absname("./lib/inputs/day_three.txt")
+    input = Path.absname(@real_input)
     result = Aoc.DayThree.part_one(input)
     assert result == 244
   end
 
+  test "part two (fake)" do
+    input = Path.absname(@fake_input)
+    result = Aoc.DayThree.part_two(input)
+    assert result == 336
+  end
+
+  test "part two" do
+    input = Path.absname(@real_input)
+    result = Aoc.DayThree.part_two(input)
+    assert result == 9406609920
+  end
 end
