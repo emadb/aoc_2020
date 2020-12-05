2159ec5f3ba81b315b98cd8c6f7fa26060bc56f5
ema <emanuele@codiceplastico.com>
2020-12-05 08:54:01 +0100

    day 5 part 1

---
+++ lib/day_five.ex
@@ -0,0 +1,44 @@
+defmodule Aoc.DayFive do
+
+  def part_one(file_path) do
+    lines = get_input_list(file_path)
+    Enum.reduce(lines, 0, fn l, acc ->
+      r = l |> String.slice(0..6) |> get_row()
+      c = l |> String.slice(7..9) |> get_column()
+      max((r * 8) + c, acc)
+    end)
+  end
+
+  def part_two(file_path) do
+
+  end
+
+
+  defp get_input_list(file_path) do
+    file_path
+    |> File.read!()
+    |> String.split("\n")
+  end
+
+  def get_row(line) do
+    chars = String.graphemes(line)
+    do_get_row(chars, 0, 127)
+  end
+
+  def get_column(line) do
+    chars = String.graphemes(line)
+    do_get_column(chars, 0, 7)
+  end
+
+  defp do_get_row(["F"], l, h), do: div(l + h, 2)
+  defp do_get_row(["B"], _l, h), do: h
+  defp do_get_row(["F" | t], l, h), do: do_get_row(t, l, div(h+l, 2))
+  defp do_get_row(["B" | t], l, h), do: do_get_row(t, div(h+l, 2), h)
+
+  defp do_get_column(["L"], l, h), do: div(l + h, 2)
+  defp do_get_column(["R"], _l, h), do: h
+  defp do_get_column(["L" | t], l, h), do: do_get_column(t, l, div(h+l, 2))
+  defp do_get_column(["R" | t], l, h), do: do_get_column(t, div(h+l, 2), h)
+
+
+end
---
+++ test/day_five_test.exs
@@ -0,0 +1,31 @@
+defmodule Aoc.DayFiveTest do
+  use ExUnit.Case
+  @fake_input "./test/fake_inputs/day_five.txt"
+  @real_input "./lib/inputs/day_five.txt"
+
+  test "part one: get row" do
+    assert Aoc.DayFive.get_row("BFFFBBF") == 70
+    assert Aoc.DayFive.get_row("FFFBBBF") == 14
+    assert Aoc.DayFive.get_row("BBFFBBF") == 102
+  end
+
+  test "part one: get column" do
+    assert Aoc.DayFive.get_column("RRR") == 7
+    assert Aoc.DayFive.get_column("RLL") == 4
+  end
+
+  test "part one (fake)" do
+    input = Path.absname(@fake_input)
+    result = Aoc.DayFive.part_one(input)
+
+    assert result == 820
+  end
+
+  test "part one" do
+    input = Path.absname(@real_input)
+    result = Aoc.DayFive.part_one(input)
+
+    assert result == 820
+  end
+
+end
