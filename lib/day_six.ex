2d48e1bbed05ac793689ed3b7f75d636aad85a47
ema <emanuele@codiceplastico.com>
2020-12-06 06:58:05 +0100

    refactoring

--- lib/day_six.ex
+++ lib/day_six.ex
@@ -1,35 +1,28 @@
 defmodule Aoc.DaySix do
-  @all "abcdefghkjilmnopqrstuxywz"
   def part_one(file_path) do
 
-    res = file_path
+    file_path
     |> get_input_list()
-    |> Enum.reduce(0, fn g, acc ->
-      acc + (g |> String.replace("\n", "") |> String.graphemes() |> Enum.uniq() |> Enum.count())
-    end)
-
-    res
+    |> Enum.reduce(0, fn g, acc -> acc + count_uniq(g) end)
   end
 
   def part_two(file_path) do
-    res = file_path
+    file_path
     |> get_input_list()
     |> Enum.reduce(0, fn g, acc ->
-
-      case Enum.map(String.split(g, "\n"),fn s -> MapSet.new(String.graphemes(s)) end) do
-        [f | rest] ->
-          result = Enum.reduce(rest, f, fn c, acc ->
-            MapSet.intersection(acc, c)
-          end)
-          acc + Enum.count(result)
-
+      case Enum.map(String.split(g, "\n"), fn s -> MapSet.new(String.graphemes(s)) end) do
+        [f | rest] -> acc + (rest |> Enum.reduce(f, &MapSet.intersection/2) |> Enum.count())
         m -> acc + Enum.count(m)
-
       end
     end)
+ end
 
-    res
-
+  defp count_uniq(g) do
+    g
+    |> String.replace("\n", "")
+    |> String.graphemes()
+    |> Enum.uniq()
+    |> Enum.count()
   end
 
   defp get_input_list(file_path) do
