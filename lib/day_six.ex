7070855de09fe84800f19c0b30416e8b469780b4
ema <emanuele@codiceplastico.com>
2020-12-06 06:52:24 +0100

    day 6 part 2

--- lib/day_six.ex
+++ lib/day_six.ex
@@ -1,5 +1,5 @@
 defmodule Aoc.DaySix do
-
+  @all "abcdefghkjilmnopqrstuxywz"
   def part_one(file_path) do
 
     res = file_path
@@ -12,6 +12,24 @@ defmodule Aoc.DaySix do
   end
 
   def part_two(file_path) do
+    res = file_path
+    |> get_input_list()
+    |> Enum.reduce(0, fn g, acc ->
+
+      case Enum.map(String.split(g, "\n"),fn s -> MapSet.new(String.graphemes(s)) end) do
+        [f | rest] ->
+          result = Enum.reduce(rest, f, fn c, acc ->
+            MapSet.intersection(acc, c)
+          end)
+          acc + Enum.count(result)
+
+        m -> acc + Enum.count(m)
+
+      end
+    end)
+
+    res
+
   end
 
   defp get_input_list(file_path) do
--- test/day_six_test.exs
+++ test/day_six_test.exs
@@ -14,6 +14,20 @@ defmodule Aoc.DaySixTest do
     input = Path.absname(@real_input)
     result = Aoc.DaySix.part_one(input)
 
-    assert result == 11
+    assert result == 6437
+  end
+
+  test "part two (fake)" do
+    input = Path.absname(@fake_input)
+    result = Aoc.DaySix.part_two(input)
+
+    assert result == 6
+  end
+
+  test "part two" do
+    input = Path.absname(@real_input)
+    result = Aoc.DaySix.part_two(input)
+
+    assert result == 3229
   end
 end
