af793a71f72b98d2c283dc7bcf81e84ffe917a63
ema <emanuele@codiceplastico.com>
2020-12-08 08:26:43 +0100

    day 8 part 2

--- lib/day_eight.ex
+++ lib/day_eight.ex
@@ -1,11 +1,49 @@
 defmodule Aoc.DayEight do
 
   def part_one(file_path) do
-
     file_path
     |> get_input_list()
     |> Enum.map(&parse_line/1)
     |> execute(0, 0)
+  end
+
+  def part_two(file_path) do
+    {:end, value} = file_path
+    |> get_input_list()
+    |> Enum.map(&parse_line/1)
+    |> execute_with_replace()
+    |> find_end()
+
+    value
+  end
+
+  defp find_end(execution) do
+    Enum.find(execution, fn ex ->
+        case ex do
+          {cmd, _} -> cmd == :end
+          _ -> false
+        end
+      end)
+  end
+
+  defp execute_with_replace(program) do
+    Enum.with_index(program, 0)
+    |> Enum.map(fn {cmd, line} ->
+      case cmd do
+        {:nop, value, ex } ->
+          updated_program = replace(program, line, {:jmp, value, ex})
+          execute(updated_program, 0, 0)
+        {:jmp, value, ex} ->
+          updated_program = replace(program, line, {:nop, value, ex})
+          execute(updated_program, 0, 0)
+        cmd -> cmd
+      end
+    end)
+  end
+
+  defp replace(program, line, cmd) do
+    {first, [_ | rest]} = Enum.split(program, line)
+    first  ++ [cmd | rest]
   end
 
   defp execute(program, acc, sp) do
@@ -14,6 +52,7 @@ defmodule Aoc.DayEight do
       {:acc, value, false} -> program |> mark_done(sp) |> execute(acc + value, sp + 1)
       {:jmp, value, false} -> program |> mark_done(sp) |> execute(acc, sp + value)
       {_, _, true} -> acc
+      nil -> {:end, acc}
     end
   end
 
@@ -22,7 +61,6 @@ defmodule Aoc.DayEight do
     {cmd, value, false} = op
     first  ++ [{cmd, value, true} | rest]
   end
-
 
   defp parse_line(line) do
     re = ~r/^(?<cmd>\w{3})\s(?<value>[+-]\d*)$/
--- test/day_eight_test.exs
+++ test/day_eight_test.exs
@@ -16,4 +16,19 @@ defmodule Aoc.DayEightTest do
 
     assert result == 1614
   end
+
+  test "part two (fake)" do
+    input = Path.absname(@fake_input)
+    result = Aoc.DayEight.part_two(input)
+
+    assert result == 8
+  end
+
+
+  test "part two" do
+    input = Path.absname(@real_input)
+    result = Aoc.DayEight.part_two(input)
+
+    assert result == 1260
+  end
 end
