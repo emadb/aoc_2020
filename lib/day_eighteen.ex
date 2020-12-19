94708621de3b3b35eeca8a76b072cf4a45a94481
ema <emanuele@codiceplastico.com>
2020-12-19 09:46:53 +0100

    day 18 part 1

--- lib/day_eighteen.ex
+++ lib/day_eighteen.ex
@@ -1,31 +1,61 @@
 defmodule Aoc.DayEighteen do
 
   def part_one(file_path) do
-
+    file_path
+    |> get_input_list()
+    |> Enum.reduce(0, fn exp, acc -> acc + calc(exp) end)
   end
-
 
   def calc(expression) do
-    [first | rest ] = prepare(expression)
-    {res, _} = Enum.reduce(rest, {first, nil}, fn x, {prev, op} ->
-      case x do
-        "+" -> {prev, &sum/2}
-        "*" -> {prev, &mul/2}
-        _   -> {op.(prev, x), nil}
-      end
-    end)
+    {res, _} = expression
+      |> prepare()
+      |> do_calc([], {0, nil})
     res
   end
+
+  defp do_calc(["(" | tail], stack, {res, op}) do
+    do_calc(tail, [{res, op} | stack], {0, nil})
+  end
+
+  defp do_calc([")" | tail], [{0, nil} | stack], {res, op}) do
+    do_calc(tail, stack, {res, op})
+  end
+
+  defp do_calc([")" | tail], [{n, op} | stack], {res, _}) do
+    do_calc(tail, stack, {op.(res, n), nil})
+  end
+
+  defp do_calc(["+" | tail], stack, {n, _}) do
+    do_calc(tail, stack, {n, &sum/2})
+  end
+
+  defp do_calc(["*" | tail], stack, {n, _}) do
+    do_calc(tail, stack, {n, &mul/2})
+  end
+
+  defp do_calc([n | tail], stack, {_, nil}) do
+    do_calc(tail, stack, {n, nil})
+  end
+
+  defp do_calc([n | tail], stack, {res, op}) do
+    do_calc(tail, stack, {op.(res, n), nil})
+  end
+
+  defp do_calc([], _, res), do: res
 
   defp sum(a, b), do: a + b
   defp mul(a, b), do: a * b
 
   defp prepare(expression) do
-    expression
+    "0 + " <> expression
+    |> String.replace(")", " )")
+    |> String.replace("(", "( ")
     |> String.split(" ")
     |> Enum.map(
       fn "+" -> "+"
          "*" -> "*"
+         "(" -> "("
+         ")" -> ")"
           x -> String.to_integer(x)
     end)
   end
--- test/day_eighteen_test.exs
+++ test/day_eighteen_test.exs
@@ -13,10 +13,37 @@ defmodule Aoc.DayEighteenTest do
     assert result == 51
   end
 
+  test "part one calc 3" do
+    result = Aoc.DayEighteen.calc("2 * 3 + (4 * 5)")
+    assert result == 26
+  end
+
+  test "part one calc 4" do
+    result = Aoc.DayEighteen.calc("5 + (8 * 3 + 9 + 3 * 4 * 3)")
+    assert result == 437
+  end
+
+  test "part one calc 5" do
+    result = Aoc.DayEighteen.calc("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")
+    assert result == 12240
+  end
+
+  test "part one calc 6" do
+    result = Aoc.DayEighteen.calc("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
+    assert result == 13632
+  end
+
   test "part one (fake)" do
     input = Path.absname(@fake_input)
-    result = Aoc.DayEighteen.calc("1 + 2 * 3 + 4 * 5 + 6")
+    result = Aoc.DayEighteen.part_one(input)
 
-    assert result == 71
+    assert result == 51
+  end
+
+  test "part one" do
+    input = Path.absname(@real_input)
+    result = Aoc.DayEighteen.part_one(input)
+
+    assert result == 51
   end
 end
